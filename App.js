4171281e384df61a8afe7149c89b0d41653c3a0e
TahhaSarwar1110 <tahasarwar1110@gmail.com>
2025-01-31 00:43:45 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import Chatbot from "./Chatbot";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <Chatbot />
+    </div>
+  );
+}
+
+export default App;
