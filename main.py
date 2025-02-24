8f8330c24736c5231d5f493ac447882ab1cbd3de
TahhaSarwar1110 <tahasarwar1110@gmail.com>
2025-02-25 00:27:46 +0500

    Update main.py

--- main.py
+++ main.py
@@ -123,7 +123,7 @@ def extract_must_do_actions(context):
 def detect_intent(user_input):
     intents = {
         "add employee": [
-            "add a new employee to my team?", "officially add", "setup a profile","register a new staff member", "correct way to add him","correct way to add her",
+            "add a new employee to my team", "setup a new employee account", "setup his account", "setup her account", "officially add", "setup a profile","register a new staff member", "correct way to add him","correct way to add her",
             "steps to add an employee", "steps to setup an account", "setup an account","give him system access", "add him", input an employee, "input him", "input her",
             "add an employee", "bring a new employee", "correct way to add an employee", "new employee", "register a new staff member", "input new team members", "input new hire"
             "register an employee","give system access", "officially add someone", "add a staff", "create an employee profile", "add a new hire"
